6b6ea86438b2efa6106004ac11b8828d8327a6c3
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-03-05 01:46:05 +0530

    Power of 2

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 
 using namespace std;
-int factorial (int n)
+int power(int n)
 {
-    // base case
-    if (n==0)
+    //base case
+    if(n==0)
     {
         return 1;
-
     }
+    // recursive case
+    int smallerproblem=power(n-1);
+    int biggerproblem=2*smallerproblem;
+    return biggerproblem;
 
-    // Recursive case
-    int chotiprob= factorial(n-1);
-    int badiprob=n*chotiprob;
-    return badiprob;
-
-    // without storing
-    // return n*factorial(n-1);
 }
 int main()
 {
     int n;
     cin>>n;
-    int ans=factorial(n);
+    int ans=power(n);
     cout<<ans<<endl;
+
     return 0;
 }
